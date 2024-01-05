40a182cb4831f87603d7194d60c90890ace359a9
Iqbal <iqbamahbub420@gmail.com>
2024-01-05 07:46:22 +0600

    first

---
+++ Module 2.dart
@@ -0,0 +1,32 @@
+void main(){
+  var myCitySet=<String>{'Dhaka','raj'};
+  print(myCitySet);
+  Map<int,String> student={
+    1:'Iqbal',2:'mahbub', 5:'Tumi',8:'ami'
+  };
+  print(student);
+  print(student.isEmpty);
+  print(student.length);
+  print(student.isNotEmpty);
+  print(student.hashCode);
+  print(student.entries);
+  Map< int , Map<String,String>>studentHistry={
+    1:{
+      'Name': 'Iqbal',
+      'FatherName':'Afzal',
+      'MotherName' : 'Anjuma'
+    },
+  2:{
+    'Name': 'Tumi',
+    'FatherName':'ami',
+    'MotherName' : 'she'
+  }
+  };
+  print(studentHistry);
+  const double x=5.88;
+  print(x);
+  final a;
+  a=10;
+  print(a);
+
+}
---
+++ bin/untitled.dart
@@ -0,0 +1,6 @@
+//import 'package:untitled/untitled.dart' as untitled;
+
+void main(List<String> arguments) {
+  print('Hello world');
+  print("name :Iqbal Mahbub");
+}
